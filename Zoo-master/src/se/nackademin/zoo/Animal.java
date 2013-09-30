2319a6abccaaf0f630b54a05a2756fe4b9a5e4da
Curwer <niclas.hjalmarsson@gmail.com>
2013-09-30 11:21:51 +0200

    bajs animl
    bajs

--- Zoo-master/src/se/nackademin/zoo/Animal.java
+++ Zoo-master/src/se/nackademin/zoo/Animal.java
@@ -2,10 +2,11 @@ package se.nackademin.zoo;
 
 public interface Animal {
 	
+	//This is your life from now on!
+	
 	public int legs();
 	public String getTitle();
 	public String getRawr();
 	public int age();
 	public String getName();
-	
 }
