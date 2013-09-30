b18266bd1414983f399237c039a5df28e3dc6059
Curwer <niclas.hjalmarsson@gmail.com>
2013-09-30 11:11:10 +0200

    Animal
    fixat interface animal

---
+++ Zoo-master/src/se/nackademin/zoo/Animal.java
@@ -0,0 +1,11 @@
+package se.nackademin.zoo;
+
+public interface Animal {
+	
+	public int legs();
+	public String getTitle();
+	public String getRawr();
+	public int age();
+	public String getName();
+	
+}
