02d1cf6a269bcee0184d1d270dafd7260ae5794f
Tjabo <carl.ryttarson@yh.nackademin.se>
2013-09-30 13:25:33 +0200

    Made stuff happen
    Added some simple calls for methods in BananFluga.java to test it all
    out.

--- src/se/nackademin/zoo/Main.java
+++ src/se/nackademin/zoo/Main.java
@@ -2,6 +2,16 @@ package se.nackademin.zoo;
 
 public class Main {
 	public static void main(String[] args){
+		
+		BananFluga bananFluga = new BananFluga("Knurra", 2);
+		BananFluga bananFluga1 = new BananFluga("Berit", 8);
+		
+		bananFluga.eat();
+		bananFluga1.eat();
+		bananFluga.play();
+		bananFluga1.play();
+		System.out.println(bananFluga.getTitle() + "n " + bananFluga.getName() + " är " + bananFluga.age() + " år gammal.");
+		System.out.println(bananFluga1.getTitle() + "n " + bananFluga1.getName() + " är " + bananFluga1.age() + " år gammal.");
 
 	}
 }
