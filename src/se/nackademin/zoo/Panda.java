555b39f3fe429b20d422d8d0da3956b3f5a2c826
Tjabo <carl.ryttarson@yh.nackademin.se>
2013-09-30 13:31:24 +0200

    Revert "Made stuff happen"
    This reverts commit 02d1cf6a269bcee0184d1d270dafd7260ae5794f.

---
+++ src/se/nackademin/zoo/Panda.java
@@ -0,0 +1,25 @@
+package se.nackademin.zoo;
+
+public class Panda implements Animal {
+
+	public int legs() {
+		return 4;
+	}
+
+	public String getTitle() {
+		return "Panda";
+	}
+
+	public String getRawr() {
+		return "ROOOOAR!";
+	}
+
+	public int age() {
+		return 8;
+	}
+
+	public String getName() {
+		return "Pandemonium";
+	}
+
+}
