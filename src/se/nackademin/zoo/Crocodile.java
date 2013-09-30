8a648eecb52713d18f7e1ea51e74ded38defb8d4
Sebastian Maneiro <sebylou@gmail.com>
2013-09-30 14:01:38 +0200

    jas

--- src/se/nackademin/zoo/Crocodile.java
+++ src/se/nackademin/zoo/Crocodile.java
@@ -1,5 +1,34 @@
 package se.nackademin.zoo;
 
-public class Crocodile {
+public class Crocodile implements Animal {
+
+	
+	public int legs() {
+		
+		return 4;
+	}
+	public String getTitle() {
+		
+		return "I'm Reptile";
+	}
+
+	
+	public String getRawr() {
+		
+		return "argh!";
+	}
+
+	
+	public int age() {
+		
+		return 47;
+	}
+
+	
+	public String getName() {
+		
+		return "Crok";
+	}
+	
 
 }
