12f1b0e5fb2ff1584864cc2f1edeaadb7f0bbdd1
prommebr0r <olle_1994@hotmail.com>
2013-09-30 11:27:39 +0200

    Animal interfaace

--- src/se/nackademin/zoo/Animal.java
+++ src/se/nackademin/zoo/Animal.java
@@ -1,7 +1,12 @@
 package se.nackademin.zoo;
 
-public class Animal {
-	public void Eat(){
-		System.out.println("I am eating! :)");
-	}
+public interface Animal {
+	
+	//This is your life from now on!
+	
+	public int legs();
+	public String getTitle();
+	public String getRawr();
+	public int age();
+	public String getName();
 }
