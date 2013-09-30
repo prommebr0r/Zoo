10f365016c18f9b2665f2cb5631bc2ebe4b91709
Mattee <nyanvandare@Mattias-MacBook.local>
2013-09-30 10:35:41 +0200

    lol
    lol

--- src/se/nackademin/zoo/Main.java
+++ src/se/nackademin/zoo/Main.java
@@ -2,7 +2,11 @@ package se.nackademin.zoo;
 
 public class Main {
 	public static void main(String args[]){
-		
+<<<<<<< HEAD
+		// Test
+=======
+		System.out.println("lol");
+>>>>>>> 57fc508b21cced03c2a8508602d4dd5ec4205c99
 	}
 
 }
