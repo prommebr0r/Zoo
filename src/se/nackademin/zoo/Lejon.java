c6520fee2fc3e7f008bb5b555ec18c8616c65573
Mattee <nyanvandare@Mattias-MacBook.local>
2013-09-30 11:41:49 +0200

    Ny Lejon

--- src/se/nackademin/zoo/Lejon.java
+++ src/se/nackademin/zoo/Lejon.java
@@ -2,29 +2,23 @@ package se.nackademin.zoo;
 
 public class Lejon implements Animal{
 
-	@Override
 	public int legs() {
 		return 4;
 	}
 
-	@Override
 	public String getTitle() {
 		return "Lejon";
 	}
 
-	@Override
 	public String getRawr() {
 		return "RAAAAAAAAAAR";
 	}
 
-	@Override
 	public int age() {
 		return 6;
 	}
 
-	@Override
 	public String getName() {
 		return "Simba";
 	}
-
 }
