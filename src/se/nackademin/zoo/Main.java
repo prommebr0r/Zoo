f5011c3ed695cc10e4b6dc9a94e2b9689b8b62a0
Tjabo <carl.ryttarson@yh.nackademin.se>
2013-10-02 10:30:05 +0200

    Fixed
    Vet inte vad ni pysslar med. Men jag tror jag fixade skiten nu.

--- src/se/nackademin/zoo/Main.java
+++ src/se/nackademin/zoo/Main.java
@@ -2,7 +2,7 @@ package se.nackademin.zoo;
 import se.nackademin.zoo.Presentation;
 public class Main {
 	public static void main(String[] args){
-<<<<<<< ours
+
 		Panda panda = new Panda();
 		Imp imp = new Imp();
 		Lejon lejon = new Lejon();
@@ -15,7 +15,7 @@ public class Main {
 		System.out.println("");
 		
 		Presentation press = new Presentation();
-<<<<<<< HEAD
+		
 		//lol xd
 		press.presentation(panda.getTitle(), panda.age(), panda.getRawr(), panda.getName(), panda.legs(), panda.getOwner());
 		press.presentation(imp.getTitle(), imp.age(), imp.getRawr(), imp.getName(), imp.legs(), imp.getOwner());
@@ -23,20 +23,14 @@ public class Main {
 		press.presentation(bananfluga.getTitle(), bananfluga.age(), bananfluga.getRawr(), bananfluga.getName(), bananfluga.legs(), bananfluga.getOwner());
 		press.presentation(zebra.getTitle(), zebra.age(), zebra.getRawr(), zebra.getName(), zebra.legs(), zebra.getOwner());
 		press.presentation(croc.getTitle(), croc.age(), croc.getRawr(), croc.getName(), croc.legs(), croc.getOwner());
-=======
-		//lol
+
+		/*
 		press.presentation(panda.getTitle(), panda.age(), panda.getRawr(), panda.getName(), panda.legs());
 		press.presentation(imp.getTitle(), imp.age(), imp.getRawr(), imp.getName(), imp.legs());
 		press.presentation(lejon.getTitle(), lejon.age(), lejon.getRawr(), lejon.getName(), lejon.legs());
 		press.presentation(bananfluga.getTitle(), bananfluga.age(), bananfluga.getRawr(), bananfluga.getName(), bananfluga.legs());
 		press.presentation(zebra.getTitle(), zebra.age(), zebra.getRawr(), zebra.getName(), zebra.legs());
 		press.presentation(croc.getTitle(), croc.age(), croc.getRawr(), croc.getName(), croc.legs());
-=======
-
-		
-		
-		
->>>>>>> theirs
->>>>>>> ok
+		*/
 	}
 }//lol
