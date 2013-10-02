d6c713a55ad9428a453a988a8168ee61e71b3d23
Sebastian Maneiro <sebylou@gmail.com>
2013-10-02 09:49:56 +0200

    ok

--- src/se/nackademin/zoo/Crocodile.java
+++ src/se/nackademin/zoo/Crocodile.java
@@ -8,8 +8,23 @@ public class Crocodile implements Animal {
 	}
 
 	public String getTitle() {
+<<<<<<< HEAD
 
 		return "Reptile";
+=======
+		String s = ""
+				+"\r            .-._   _ _ _ _ _ _ _ _"
+				+"\r .-''-.__.-'00  '-' ' ' ' ' ' ' ' '-."
+				+"\r'.___ '    .   .--_'-' '-' '-' _'-' '._"
+				+"\r V: V 'vv-'   '_   '.       .'  _..' '.'."
+				+"\r   '=.____.=_.--'   :_.__.__:_   '.   : :"
+				+"\r           (((____.-'        '-.  /   : :"
+				+"\r                             (((-'  .'   "
+				+"\r                           _____..'  .'"
+				+"\r                          '-._____.-'";				
+   
+		return "I'm Reptile and look like this: "+s;
+>>>>>>> ok
 	}
 
 	public String getRawr() {
